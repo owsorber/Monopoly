ec2f0b755678975202d5d2272ab2de0d7a9a2d82
Jack Williamson <jew256@cornell.edu>
2021-04-07 11:35:33 -0400

    Added doubles implementation

--- main.ml
+++ main.ml
@@ -1,22 +1,63 @@
 open Printers
 
+(**[take_action result p g phase] executes the action given by result
+   [result], if one exists, on player [p] given game [g] during phase
+   [phase]. *)
 let take_action result p g phase =
   match result with
-    | Input.Legal r ->
-        Input.get_action r p;
-        if phase = false then (
-          Game.next_player g;
-          red_print "Press enter to begin next player's turn. ";
-          let _ = (read_line ()) in true)
-        else true;
-    | Input.Illegal -> red_print "Illegal move. Please enter a valid move.\n";
+  | Input.Legal r ->
+      Input.get_action r p;
+      if phase = false then (
+        Game.next_player g;
+        red_print "Press enter to begin next player's turn. ";
+        let _ = read_line () in
+        true)
+      else true
+  | Input.Illegal ->
+      red_print "Illegal move. Please enter a valid move.\n";
       false
 
+(**[double_turn b g p] handles taking another roll phase for player [p]
+   given board [b] and game [g]. *)
+let rec double_turn b g p =
+  let result = Input.turn p b g true in
+  let _ = take_action result p g true in
+  match result with
+  | Input.Legal r ->
+      let double = Input.get_double r in
+      if double then
+        green_print
+          "WOW! Doubles again?! Sadly that's it for rolling. \n\n"
+      else ()
+  | Input.Illegal ->
+      red_print "Illegal move. Please enter a valid move. \n";
+      double_turn b g p
+
+(**[handle_double result b g p] executes a call to [double turn] if a
+   double was rolled by player [p], given result [result], board [b],
+   and game [g]. *)
+let handle_double result b g p =
+  match result with
+  | Input.Legal r ->
+      let double = Input.get_double r in
+      if double then (
+        (*redo phase 1*)
+        green_print "Yay! You rolled doubles. You may roll again! \n";
+        double_turn b g p)
+      else ()
+  | Input.Illegal -> ()
+(*already handled*)
+
+(**[turn b g phase] repeatedly executes turns for each player, beginning
+   on phase [phase] advancing to the next player and phase after every
+   turn. Turns are executed on board [b] and game [g]*)
 let rec turn b g phase =
   (*do current player turn*)
   let current_player = Game.current_player g in
-  let move = Input.turn current_player b g phase in
-  let progress = take_action move current_player g phase in
+  let result = Input.turn current_player b g phase in
+  let progress = take_action result current_player g phase in
+  (*check for doubles*)
+  handle_double result b g current_player;
   (*advance to next player in game*)
   match progress with
   | true -> turn b g (not phase)
