ac66016bf81e50d28864875cb02ef1887e0d4ee1
Kira <kms389@cornell.edu>
2021-04-11 15:00:46 -0400

    updated ml, fail w unimplemented

--- player.ml
+++ player.ml
@@ -8,6 +8,10 @@ type property_name_list = string list
 
 type rolled_dice = int * int
 
+type quarantine_status = 
+|In of int
+|Out
+
 exception BalanceBelowZero
 
 type t = {
@@ -15,6 +19,7 @@ type t = {
   mutable balance : balance;
   mutable location : location;
   mutable property_name_list : property_name_list;
+  mutable quarantine_status : quarantine_status;
 }
 
 let get_player_id player = player.player_id
@@ -31,6 +36,7 @@ let make_player id =
     balance = 1500;
     location = 0;
     property_name_list = [];
+    quarantine_status = Out
   }
 
 (* [sums roll] is the sum of the two value die in [roll] *)
@@ -49,10 +55,18 @@ let update_balance player i =
 let passes_go roll player = player.location + sums roll >= 40
 
 let move_player roll player =
-  if passes_go roll player then player.balance <- player.balance + 200;
-  let new_pos = (player.location + sums roll) mod 40 in
-  player.location <- new_pos
+  if passes_go roll player then player.balance <- player.balance + 200; 
+  let new_pos = (player.location + sums roll) mod 40 in player.location 
+  <- new_pos
 
 let projected_space roll player board =
   let new_pos = (player.location + sums roll) mod 40 in
   Board.space_name board new_pos
+let go_to_quarantine_status = failwith "unimplemented"
+let decrement_day_quarantine = failwith "unimplemented"
+let quarantine = failwith "unimplemented"
+let buy_property =failwith "unimplemented"
+let mortgage_property = failwith "unimplemented"
+let pay p1 p2 i =
+  update_balance p1 (-i);
+  update_balance p2 (i);
