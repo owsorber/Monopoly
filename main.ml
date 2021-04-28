4d49e200ab886d680d8a9c55be4f4814b0c02bba
Jack Williamson <jew256@cornell.edu>
2021-04-28 15:39:59 -0400

    Added test games

--- main.ml
+++ main.ml
@@ -105,33 +105,97 @@ let rec get_player_count () =
     print_string "Invalid input. Please enter a positive integer. \n";
     get_player_count ()
 
+let default_game () =
+  let board = Board.init_board (Yojson.Basic.from_file "board.json") in
+  (*query number of players*)
+  let n = get_player_count () in
+  let players = Array.make n (Player.make_player "") in
+  for i = 1 to n do
+    players.(i - 1) <-
+      Player.make_player
+        (yellow_print ("Enter Player " ^ string_of_int i ^ "'s name: ");
+         read_line ())
+  done;
+  Game.init_game board players
+
+let game1 () =
+  let board = Board.init_board (Yojson.Basic.from_file "board.json") in
+  let p1 = Player.make_player "player1" in
+  Player.update_balance p1 10000;
+  let g = Game.init_game board [| p1 |] in
+  Player.buy_ownable p1 "Mediterranean Avenue" 60;
+  Game.make_ownable_owned g p1 "Mediterranean Avenue";
+  Player.buy_ownable p1 "Baltic Avenue" 60;
+  Game.make_ownable_owned g p1 "Baltic Avenue";
+  g
+
+(* buys a list of ownables for a player*)
+let rec buy_ownable_lst game board player name_lst =
+  match name_lst with
+  | [] -> ()
+  | h :: t ->
+      Player.buy_ownable player h (Game.get_ownable_price board h);
+      Game.make_ownable_owned game player h;
+      buy_ownable_lst game board player t
+
+let ownable_lst_of_default_board board =
+  let lst = ref [] in
+  for i = 0 to 39 do
+    let space_name = Board.space_name board i in
+    match Board.space_from_space_name board space_name with
+    | Some space ->
+        if Board.is_ownable board space then lst := space_name :: !lst
+        else ()
+    | None -> ()
+  done;
+  !lst
+
+let game2 () =
+  let board = Board.init_board (Yojson.Basic.from_file "board.json") in
+  let p1 = Player.make_player "player1" in
+  let p2 = Player.make_player "player2" in
+  Player.update_balance p1 (-1500);
+  Player.update_balance p2 1000000;
+  let ownable_lst = ownable_lst_of_default_board board in
+  let g = Game.init_game board [| p1; p2 |] in
+  buy_ownable_lst g board p2 ownable_lst;
+  g
+
+let games = [| default_game; game1; game2 |]
+
+let game_choices () =
+  yellow_print "1: ";
+  white_print "New Game.";
+  yellow_print "2: ";
+  white_print "Test game. Capable of buying house.";
+  yellow_print "3: ";
+  white_print
+    "Test game. Player1 has zero balance and Player2 owns all ownables."
+
 (** [play_game b] starts the game given board file f. *)
 let rec play_game () =
-  print_string
-    "Please enter the name of the board file you want to load.\n";
+  print_string "Please enter the number of the game you want to play.\n";
+  game_choices ();
   print_string "> ";
-  match read_line () with
-  | exception End_of_file -> ()
-  | f -> (
-      try
-        let board = Board.init_board (Yojson.Basic.from_file f) in
-        (*query number of players*)
-        let n = get_player_count () in
-        let players = Array.make n (Player.make_player "") in
-        for i = 1 to n do
-          players.(i - 1) <-
-            Player.make_player
-              (yellow_print
-                 ("Enter Player " ^ string_of_int i ^ "'s name: ");
-               read_line ())
-        done;
-        (* create board with number of players *)
-        let game = Game.init_game board players in
-        (* turn board game true *)
-        turn_handler board game
-      with Sys_error _ ->
-        Stdlib.print_endline "board file not found";
-        play_game ())
+  try
+    let index = int_of_string (read_line ()) in
+    let game = games.(index - 1) in
+    let board =
+      Board.init_board (Yojson.Basic.from_file "board.json")
+    in
+    turn_handler board (game ())
+  with _ ->
+    red_print "Please enter a valid index.\n";
+    play_game ()
+(* match read_line () with | exception End_of_file -> () | f -> ( try
+   let board = Board.init_board (Yojson.Basic.from_file f) in (*query
+   number of players*) let n = get_player_count () in let players =
+   Array.make n (Player.make_player "") in for i = 1 to n do players.(i
+   - 1) <- Player.make_player (yellow_print ("Enter Player " ^
+   string_of_int i ^ "'s name: "); read_line ()) done; (* create board
+   with number of players *) let game = Game.init_game board players in
+   (* turn board game true *) turn_handler board game with Sys_error _
+   -> Stdlib.print_endline "board file not found"; play_game ()) *)
 
 (** [main ()] prompts for the board to use, then starts it. *)
 let main () =
